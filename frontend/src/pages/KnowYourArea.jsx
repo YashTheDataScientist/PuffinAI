e3f48f899539a20a91ccac4f9f6303ed3d00601e
Fatsoo80 <yapottabathini97@gmail.com>
2025-05-02 04:45:31 +1000

    moved more data to public

--- frontend/src/pages/KnowYourArea.jsx
+++ frontend/src/pages/KnowYourArea.jsx
@@ -110,7 +110,7 @@ const KnowYourArea = () => {
 
   useEffect(() => {
     if (matchedSuburb) {
-      Papa.parse('/src/assets/All_Plants_Victoria_With_suburbs.csv', {
+      Papa.parse('data/All_Plants_Victoria_With_suburbs.csv', {
         download: true,
         header: true,
         complete: (results) => {
